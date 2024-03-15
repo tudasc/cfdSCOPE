22958907619d63a2c77f3100dab3edf71b641345
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-15 18:09:18 +0100

    added basic IO test

--- src/MiniCFD.cpp
+++ src/MiniCFD.cpp
@@ -4,9 +4,6 @@
 int main(int argc, char** argv) {
     // Command line parsing (later)
 
-    auto pair = read_from_file<double>("/home/tim/peng-prak-code-mini-cfd/sample_io");
-    write_to_file(pair.first,pair.second,"/home/tim/peng-prak-code-mini-cfd/sample_io_out");
-
     // Set up grid
 
     // Time loop
---
+++ tests/io_basic_test.cpp
@@ -0,0 +1,21 @@
+#include "IO.h"
+#include <cstdio>
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(IoBasicTest, BasicAssertions) {
+
+    std::string filename = std::string(TEST_RESOURCE_DIR) + "/sample_io";
+    auto pair = read_from_file<double>(filename);
+    auto filename_out = filename + "_out";
+    write_to_file(pair.first, pair.second, filename_out);
+
+    std::ifstream f1(filename_out);
+    std::ifstream f2(filename);
+
+    ASSERT_TRUE(std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
+                           std::istreambuf_iterator<char>(),
+                           std::istreambuf_iterator<char>(f2.rdbuf())));
+    // clean up
+    std::remove(filename_out.c_str());
+}
