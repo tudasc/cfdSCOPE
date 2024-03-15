0d85d35d933204aeb22bd25509c6e5bcd5e890a5
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-15 15:24:36 +0100

    added 3D

--- Plot_test.py
+++ Plot_test.py
@@ -12,78 +12,79 @@ def show_3d_plot():
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    x, y, z = np.meshgrid(np.arange(-0.8, 1, 0.2),
-                          np.arange(-0.8, 1, 0.2),
-                          np.arange(-0.8, 1, 0.8))
+    x, y, z = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
 
-    u = np.sin(np.pi * x) * np.cos(np.pi * y) * np.cos(np.pi * z)
-    v = -np.cos(np.pi * x) * np.sin(np.pi * y) * np.cos(np.pi * z)
-    w = (np.sqrt(2.0 / 3.0) * np.cos(np.pi * x) * np.cos(np.pi * y) *
-         np.sin(np.pi * z))
+    u = -y / np.sqrt(x ** 2 + y ** 2)
+    v = x / np.sqrt(x ** 2 + y ** 2)
+    w = np.zeros(shape=u.shape)
 
-    ax.quiver(x, y, z, u, v, w, length=0.1, color='black')
+    FRAMES = 50
 
-    plt.show()
+    data = get_plot_slices_3d(x, y, z, u, v, w,num_slices=FRAMES)
 
 
-def animated_2d_plot():
-    X, Y = np.mgrid[:2 * np.pi:10j, :2 * np.pi:5j]
-    U = np.cos(X)
-    V = np.sin(Y)
 
-    fig, ax = plt.subplots(1, 1)
-    Q = ax.quiver(X, Y, U, V, pivot='mid', color='r', units='inches')
-
-    ax.set_xlim(-1, 7)
-    ax.set_ylim(-1, 7)
-
-    def update_quiver(num, Q, X, Y):
-        """updates the horizontal and vertical vector components by a
-        fixed increment on each frame
-        """
-
-        U = np.cos(X + num * 0.1)
-        V = np.sin(Y + num * 0.1)
-
-        Q.set_UVC(U, V)
-
-        return Q,
-
-    # you need to set blit=False, or the first set of arrows never gets
-    # cleared on subsequent frames
-    anim = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y),
-                                   interval=50, blit=False)
-    fig.tight_layout()
-    plt.show()
-
-
-def test_animation():
-    x1 = np.arange(0, -0.2, -0.002)
-    y1 = np.arange(0, -0.2, -0.002)
-    x2 = np.arange(3.9, 3.7, -0.002)
-    y2 = np.arange(0, 1, 0.01)
-    x3 = np.arange(0, 1.8, 0.018)
-    y3 = np.array(x3 ** 2)
-
-    fig, ax = plt.subplots()
-
-    def animate(i):
+    def animate(i, plot_data):
         ax.clear()
-        ax.set_xlim(-4, 4)
-        ax.set_ylim(-4, 4)
-        line, = ax.plot(x1[0:i], y1[0:i], color='blue', lw=1)
-        line2, = ax.plot(x2[0:i], y2[0:i], color='red', lw=1)
-        line3, = ax.plot(x3[0:i], y3[0:i], color='purple', lw=1)
-        point1, = ax.plot(x1[i], y1[i], marker='.', color='blue')
-        point2, = ax.plot(x2[i], y2[i], marker='.', color='red')
-        point3, = ax.plot(x3[i], y3[i], marker='.', color='purple')
-        return line, line2, line3, point1, point2, point3,
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-5, 5)
+        ax.set_zlim(-5, 5)
+        x, y, z, u, v, w = plot_data[i]
+        q = ax.quiver(x, y, z, u, v, w)
 
-    ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=100)
+        return q,
+
+    print("calculated interpolation, plotting data...")
+
+    ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=FRAMES, fargs=[data])
     ani.save("TLI.gif", dpi=300, writer=PillowWriter(fps=25))
+    print("3D plot")
+    exit()
 
 
-def get_plot_slices(x, y, u, v, num_slices=100, slice_step=10,movement_factor =0.1):
+def get_plot_slices_3d(x, y, z, u, v, w, num_slices=100, slice_step=10, movement_factor=0.1):
+    # Reshape u, v for interpolation
+    u_known_reshaped = u.reshape(-1)
+    v_known_reshaped = v.reshape(-1)
+    w_known_reshaped = w.reshape(-1)
+    points = np.column_stack((x.ravel(), y.ravel(), z.ravel()))
+
+    xx = x[0::slice_step, 0::slice_step, 0::slice_step]
+    yy = y[0::slice_step, 0::slice_step, 0::slice_step]
+    zz = z[0::slice_step, 0::slice_step, 0::slice_step]
+    uu = u[0::slice_step, 0::slice_step, 0::slice_step]
+    vv = v[0::slice_step, 0::slice_step, 0::slice_step]
+    ww = v[0::slice_step, 0::slice_step, 0::slice_step]
+
+    result = []
+    result.append((xx, yy, zz, uu, vv, ww))
+    for i in range(num_slices):
+        x_new = xx + uu * movement_factor
+        y_new = yy + vv * movement_factor
+        z_new = zz + ww * movement_factor
+        new_points = np.column_stack((x_new.ravel(), y_new.ravel(), z_new.ravel()))
+        # Interpolate new x, y,z to find corresponding u, v,w
+        u_new = scipy.interpolate.griddata(points, u_known_reshaped, new_points, method='nearest')
+        v_new = scipy.interpolate.griddata(points, v_known_reshaped, new_points, method='nearest')
+        w_new = scipy.interpolate.griddata(points, w_known_reshaped, new_points, method='nearest')
+
+        # Reshape interpolated u, v
+        u_new = u_new.reshape(x_new.shape)
+        v_new = v_new.reshape(y_new.shape)
+        w_new = w_new.reshape(z_new.shape)
+
+        xx = x_new
+        yy = y_new
+        zz = z_new
+        uu = u_new
+        vv = v_new
+        ww = w_new
+        result.append((xx, yy, zz, uu, vv, ww))
+
+    return result
+
+
+def get_plot_slices(x, y, u, v, num_slices=100, slice_step=10, movement_factor=0.1):
     xx = x[0::slice_step, 0::slice_step]
     yy = y[0::slice_step, 0::slice_step]
     uu = u[0::slice_step, 0::slice_step]
@@ -97,8 +98,8 @@ def get_plot_slices(x, y, u, v, num_slices=100, slice_step=10,movement_factor =0
         v_known_reshaped = v.reshape(-1)
         points = np.column_stack((x.ravel(), y.ravel()))
 
-        x_new = xx + uu*movement_factor
-        y_new = yy + vv*movement_factor
+        x_new = xx + uu * movement_factor
+        y_new = yy + vv * movement_factor
         new_points = np.column_stack((x_new.ravel(), y_new.ravel()))
         # Interpolate new x, y to find corresponding u, v
         u_new = scipy.interpolate.griddata(points, u_known_reshaped, new_points, method='nearest')
@@ -116,7 +117,11 @@ def get_plot_slices(x, y, u, v, num_slices=100, slice_step=10,movement_factor =0
 
     return result
 
+
 def main():
+    show_3d_plot()
+
+    exit()
     # full plot of all known forces
     x, y = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
 
@@ -134,17 +139,16 @@ def main():
         ax.set_xlim(-5, 5)
         ax.set_ylim(-5, 5)
         x, y, u, v = plot_data[i]
-        q = ax.quiver(x, y, u, v,np.linalg.norm((v, u)))
+        q = ax.quiver(x, y, u, v, np.linalg.norm((v, u)))
 
         return q,
 
-    ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=100,fargs=[data])
+    ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=100, fargs=[data])
     ani.save("TLI.gif", dpi=300, writer=PillowWriter(fps=25))
 
     print("")
 
     exit()
-
 
 
 if __name__ == '__main__':
