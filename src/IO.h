9908c1420cc73aae6b2cfdc1437c2c5477e941c2
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-15 17:02:46 +0100

    added IO implementation

---
+++ src/IO.h
@@ -0,0 +1,100 @@
+#include <cstddef>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+
+#include "Grid.h"
+
+/**
+ * 
+ * @tparam T 
+ * @param fname 
+ * @return 
+ */
+template<typename T>
+std::pair<VelocityField<T>, PressureField<T>> read_from_file(std::string fname) {
+    std::ifstream ifile;
+    ifile.open(fname);
+    std::string line;
+    // read first line
+    std::getline(ifile, line, ',');
+    size_t width = std::stoi(line);
+    std::getline(ifile, line, ',');
+    size_t height = std::stoi(line);
+    std::getline(ifile, line, '\n');
+    size_t depth = std::stoi(line);
+
+    Grid<T> grid(width, height, depth);
+    PressureField<T> pressures(grid);
+    VelocityField<T> velocities(grid);
+
+    for (size_t z = 0; z < velocities.getDepth(); ++z) {
+        std::getline(ifile, line, '\n');
+        assert(line == "");
+        for (size_t y = 0; y < velocities.getHeight(); ++y) {
+            std::getline(ifile, line, '\n');
+            std::stringstream line_stream(line);
+            for (size_t x = 0; x < velocities.getWidth(); ++x) {
+                std::getline(line_stream, line, ',');
+                T p = std::stod(line);
+                pressures.setPressure(x, y, z, p);
+                std::getline(line_stream, line, ',');
+                T u = std::stod(line);
+                velocities.setLeftU(x, y, z, u);
+                std::getline(line_stream, line, ',');
+                T v = std::stod(line);
+                velocities.setTopV(x, y, z, v);
+                std::getline(line_stream, line, ',');
+                T w = std::stod(line);
+                velocities.setFrontW(x, y, z, w);
+            }
+        }
+    }
+    ifile.close();
+
+    return std::make_pair(velocities, pressures);
+}
+
+/**
+ * 
+ * @tparam T 
+ * @param grid 
+ * @param fname 
+ */
+template<typename T>
+void write_to_file(const VelocityField<T> &velocities, const PressureField<T> &pressures, std::string fname) {
+
+    assert(velocities.getDepth() == pressures.getDepth());
+    assert(velocities.getHeight() == pressures.getHeight());
+    assert(velocities.getWidth() == pressures.getWidth());
+
+    std::ofstream ofile;
+    ofile.open(fname);
+
+    ofile << velocities.getWidth() << "," << velocities.getHeight() << "," << velocities.getDepth() << "\n\n";
+    for (size_t z = 0; z < velocities.getDepth(); ++z) {
+        for (size_t y = 0; y < velocities.getHeight(); ++y) {
+            for (size_t x = 0; x < velocities.getWidth(); ++x) {
+                ofile << "," << pressures.getPressure(x, y, z)
+                      << "," << velocities.getLeftU(x, y, z)
+                      << "," << velocities.getTopV(x, y, z)
+                      << "," << velocities.getFrontW(x, y, z);
+            }
+            ofile << "\n";
+        }
+        ofile << "\n";
+    }
+    ofile.close();
+}
+
+/* Documentation of output file:
+ * First line: grid dimensions.
+ * Followed By:
+ * Comma seperated list of values
+ * For each field:pressure, x,y,z velocities
+ * each line lists all field in x direction,
+ * the lines are written in y direction
+ * each z entry is seperated by blank lines between "y blocks"
+*/
