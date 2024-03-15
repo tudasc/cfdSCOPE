745a045a3fea6ad67bc98dd7b43657c0ff0e2f1e
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-15 17:41:18 +0100

    format code

--- src/Grid.h
+++ src/Grid.h
@@ -1,7 +1,7 @@
 #include "Vector.h"
 
 // "indexing helper"
-template<typename T>
+template <typename T>
 class Grid {
     size_t width;
     size_t height;
@@ -12,9 +12,9 @@ class Grid {
     // Vector<T> p;
     // Vector<T> v;
 
-public:
+  public:
     Grid(size_t width, size_t height, size_t depth, T resolution)
-            : width(width), height(height), depth(depth), resolution(resolution) {}
+        : width(width), height(height), depth(depth), resolution(resolution) {}
 
     size_t getWidth() const { return width; }
 
@@ -26,26 +26,24 @@ public:
         return z * width * height + y * height + x;
     }
 
-    T getResolution() const {
-        return resolution;
-    }
+    T getResolution() const { return resolution; }
 
     size_t getCellCount() const { return width * height * depth; }
 };
 
-template<typename T>
+template <typename T>
 class PressureField {
-    Grid<T> &grid;
+    Grid<T>& grid;
     Vector<T> field;
 
-public:
-    PressureField(Grid<T> &grid) : grid(grid), field(grid.getCellCount()) {}
+  public:
+    PressureField(Grid<T>& grid) : grid(grid), field(grid.getCellCount()) {}
 
-    const T &getPressure(size_t x, size_t y, size_t z) const {
+    const T& getPressure(size_t x, size_t y, size_t z) const {
         return field[grid.cellIndex(x, y, z)];
     }
 
-    void setPressure(size_t x, size_t y, size_t z, const T &val) {
+    void setPressure(size_t x, size_t y, size_t z, const T& val) {
         field[grid.cellIndex(x, y, z)] = val;
     }
 
@@ -55,74 +53,72 @@ public:
 
     size_t getDepth() const { return grid.getDepth(); }
 
-    T getResolution() const {
-        return grid.getResolution();
-    }
+    T getResolution() const { return grid.getResolution(); }
 
     Vec3<T> div(size_t x, size_t y) const {}
 };
 
-template<typename T>
+template <typename T>
 class VelocityField {
-    Grid<T> &grid;
+    Grid<T>& grid;
     Vector<T> field;
 
-public:
-    VelocityField(Grid<T> &grid) : grid(grid), field(grid.getCellCount() * 3) {}
+  public:
+    VelocityField(Grid<T>& grid) : grid(grid), field(grid.getCellCount() * 3) {}
 
     /**
      *
      */
-    const T &getLeftU(size_t x, size_t y, size_t z) const {
+    const T& getLeftU(size_t x, size_t y, size_t z) const {
         return field[grid.cellIndex(x, y, z) * 3];
     }
 
-    const T &getRightU(size_t x, size_t y, size_t z) const {
+    const T& getRightU(size_t x, size_t y, size_t z) const {
         assert(x + 1 < grid.getWidth() && "x out of bounds");
         return field[grid.cellIndex(x + 1, y, z) * 3];
     }
 
-    const T &getTopV(size_t x, size_t y, size_t z) const {
+    const T& getTopV(size_t x, size_t y, size_t z) const {
         return field[grid.cellIndex(x, y, z) * 3];
     }
 
-    const T &getBottomV(size_t x, size_t y, size_t z) const {
+    const T& getBottomV(size_t x, size_t y, size_t z) const {
         assert(y + 1 < grid.getHeight() && "y out of bounds");
         return field[grid.cellIndex(x, y + 1, z) * 3];
     }
 
-    const  T &getFrontW(size_t x, size_t y, size_t z) const {
+    const T& getFrontW(size_t x, size_t y, size_t z) const {
         return field[grid.cellIndex(x, y, z) * 3];
     }
 
-    const T &getBackW(size_t x, size_t y, size_t z) const {
+    const T& getBackW(size_t x, size_t y, size_t z) const {
         assert(z + 1 < grid.getDepth() && "z out of bounds");
         return field[grid.cellIndex(x, y, z + 1) * 3];
     }
 
-    void setLeftU(size_t x, size_t y, size_t z, const T &value) {
+    void setLeftU(size_t x, size_t y, size_t z, const T& value) {
         field[grid.cellIndex(x, y, z) * 3] = value;
     }
 
-    void setRightU(size_t x, size_t y, size_t z, const T &value) {
+    void setRightU(size_t x, size_t y, size_t z, const T& value) {
         assert(x + 1 < grid.getWidth() && "x out of bounds");
         field[grid.cellIndex(x + 1, y, z) * 3] = value;
     }
 
-    void setTopV(size_t x, size_t y, size_t z, const T &value) {
+    void setTopV(size_t x, size_t y, size_t z, const T& value) {
         field[grid.cellIndex(x, y, z) * 3] = value;
     }
 
-    void setBottomV(size_t x, size_t y, size_t z, const T &value) {
+    void setBottomV(size_t x, size_t y, size_t z, const T& value) {
         assert(y + 1 < grid.getHeight() && "y out of bounds");
         field[grid.cellIndex(x, y + 1, z) * 3] = value;
     }
 
-    void setFrontW(size_t x, size_t y, size_t z, const T &value) {
+    void setFrontW(size_t x, size_t y, size_t z, const T& value) {
         field[grid.cellIndex(x, y, z) * 3] = value;
     }
 
-    void setBackW(size_t x, size_t y, size_t z, const T &value) {
+    void setBackW(size_t x, size_t y, size_t z, const T& value) {
         assert(z + 1 < grid.getDepth() && "z out of bounds");
         field[grid.cellIndex(x, y, z + 1) * 3] = value;
     }
@@ -133,9 +129,7 @@ public:
 
     size_t getDepth() const { return grid.getDepth(); }
 
-    T getResolution() const {
-        return grid.getResolution();
-    }
+    T getResolution() const { return grid.getResolution(); }
 
     /**
      * Divergence operator using central difference.
