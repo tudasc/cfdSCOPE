d427fd5b071e2b0f1652bdbb7888523c81b0f90c
Peter Arzt <peter.arzt@tu-darmstadt.de>
2024-04-08 13:17:07 +0200

    Fix and tests for tri-linear interpolation

--- tests/trilerp_tests.cpp
+++ tests/trilerp_tests.cpp
@@ -5,8 +5,8 @@
 #include "Grid.h"
 #include "Vector.h"
 
-TEST(TrilerpTests, Basics) {
-    Vector<float> init(27, 1.0);
+TEST(TrilerpTests, Sanity) {
+    Vector<float> init(27*3, 1.0);
 
     VelocityField<float> field(std::make_shared<Grid<float>>(3, 3, 3, 1.0), init);
 
@@ -14,4 +14,30 @@ TEST(TrilerpTests, Basics) {
     ASSERT_FLOAT_EQ(t.x, 1.0);
     ASSERT_FLOAT_EQ(t.y, 1.0);
     ASSERT_FLOAT_EQ(t.z, 1.0);   
+}
+
+TEST(TrilerpTests, SimpleInterpolations) {
+    Vector<float> init(27*3, 0.0);
+
+    VelocityField<float> field(std::make_shared<Grid<float>>(3, 3, 3, 1.0), init);
+
+    field.setLeftU(1, 1, 1, 1.0);
+    field.setTopV(1, 1, 1, 1.0);
+    field.setFrontW(1, 1, 1, 1.0);
+
+    auto t = field.trilerp({1.5, 1.5, 1.5});
+    ASSERT_FLOAT_EQ(t.x, 0.5);
+    ASSERT_FLOAT_EQ(t.y, 0.5);
+    ASSERT_FLOAT_EQ(t.z, 0.5);
+
+    t = field.trilerp({1.0, 1.5, 1.5});
+    ASSERT_FLOAT_EQ(t.x, 1.0);
+    ASSERT_FLOAT_EQ(t.y, 0.25);
+    ASSERT_FLOAT_EQ(t.z, 0.25);
+
+    t = field.trilerp({1.25, 1.25, 1.25});
+    ASSERT_FLOAT_EQ(t.x, 0.75*0.75*0.75);
+    ASSERT_FLOAT_EQ(t.y, 0.75*0.75*0.75);
+    ASSERT_FLOAT_EQ(t.z, 0.75*0.75*0.75);
+
 }
